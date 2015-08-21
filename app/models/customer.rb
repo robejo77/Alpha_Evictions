bedece12b5b6f05191315183ba6b0788415cd68e
robejo77 <robejo77@hotmail.com>
2015-08-21 18:40:37 -0400

    added table relationships and join table.  Need to work on customer dropdown on eviction table

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,5 @@
 class Customer < ActiveRecord::Base
-	has_many :evictions
-
+	has_many :evictions, inverse_of: :eviction
 	def company_and_id
     "#{company} #{id}"
   	end
--- app/models/eviction.rb
+++ app/models/eviction.rb
@@ -1,3 +1,4 @@
 class Eviction < ActiveRecord::Base
-	belongs_to :customer
+	belongs_to :customer, inverse_of: :customer
+	has_many :properties
 end
